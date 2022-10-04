cadc44b6ff1ba8e0e8bb72f28e0e1a0f05c25a73
Maria Silva <misilva73@gmail.com>
2022-10-04 17:21:35 +0100

    Add code for single simulation

---
+++ saturn-aliens/src/treasury_sim/operator.py
@@ -0,0 +1,76 @@
+import numpy as np
+from typing import List, Tuple
+
+
+class Operator:
+    def __init__(self, op_type: str) -> None:
+        self.op_type: str = op_type
+        self.set_performance()
+        self.set_detection_prob()
+        self.payout_list: List[float] = []
+        self.reward_list: List[float] = []  # rewards before penalties
+        self.bandwidth_list: List[float] = []
+        self.flag_list: List[bool] = []
+        self.collateral_saved: float = 0.0
+        self.penalty: float = None
+        self.penalty_list: List[float] = []
+
+    def set_performance(self) -> None:
+        if self.op_type == "honest_high_l1":
+            self.mu_bandwidth: float = 100.0
+            self.sigma_bandwidth: float = 1.0
+            self.uptime: float = 1.0
+            self.speed_ratio: float = 1.0
+        elif self.op_type in ["honest_normal_l1", "cheating_l1"]:
+            self.mu_bandwidth: float = 80.0
+            self.sigma_bandwidth: float = 1.0
+            self.uptime: float = 1.0
+            self.speed_ratio: float = 0.9
+        elif self.op_type == "honest_low_l1":
+            self.mu_bandwidth: float = 50.0
+            self.sigma_bandwidth: float = 1.0
+            self.uptime: float = 0.9
+            self.speed_ratio: float = 0.5
+        else:
+            raise ValueError("Invalid op_type")
+
+    def set_detection_prob(self) -> None:
+        honest_ops = ["honest_high_l1", "honest_normal_l1", "honest_low_l1"]
+        cheating_ops = ["cheating_l1"]
+        if self.op_type in honest_ops:
+            self.detection_prob: float = 0.02
+        elif self.op_type in cheating_ops:
+            self.detection_prob: float = 0.25
+        else:
+            raise ValueError("Invalid op_type")
+
+    def generate_bandwidth(self) -> None:
+        bw: float = np.random.normal(self.mu_bandwidth, self.sigma_bandwidth)
+        self.bandwidth_list.append(bw)
+
+    def generate_flag(self) -> None:
+        flag: bool = np.random.choice(
+            [True, False], p=[self.detection_prob, 1 - self.detection_prob]
+        )
+        self.flag_list.append(flag)
+
+    def add_reward(self, reward: float) -> None:
+        self.reward_list.append(reward)
+        self.penalty = 5.0 * np.mean(self.reward_list)
+
+    def compute_current_payout(self) -> None:
+        is_flagged: bool = self.flag_list[-1]
+        curr_reward: float = self.reward_list[-1]
+        # Apply penalty in case of detection
+        if is_flagged:
+            self.collateral_saved -= self.penalty
+            self.penalty_list.append(self.penalty)
+        # Update saved collateral and compute payout
+        if self.collateral_saved < self.penalty:
+            missing_collateral = self.penalty - self.collateral_saved
+            payout: float = max(0.0, curr_reward - missing_collateral)
+            self.collateral_saved += curr_reward - payout
+        else:
+            payout: float = curr_reward
+        # Update payout list
+        self.payout_list.append(payout)
---
+++ saturn-aliens/src/treasury_sim/sim.py
@@ -0,0 +1,69 @@
+import numpy as np
+from typing import Callable, List
+from treasury_sim.operator import Operator
+
+
+def run_single_sim(
+    sim_len: int,
+    k1: float,
+    k2: float,
+    day_pool_list: List[float],
+    initial_ops_num: int,
+    new_ops_num: int,
+) -> List[Operator]:
+    ops_list = generate_ops(initial_ops_num)
+    for sim_step in range(sim_len):
+        # Get daily reward from initial pool
+        if sim_step < len(day_pool_list):
+            day_pool: float = day_pool_list[sim_step]
+        else:
+            day_pool: float = 0.0
+        # Update perf. metrics and log detection flags
+        update_ops_bandwidth(ops_list)
+        update_ops_flags(ops_list)
+        # Compute rewards (before penalties)
+        update_ops_rewards(ops_list, day_pool, k1, k2)
+        # Compute final payout (after penalties)
+        update_ops_payouts(ops_list)
+        # Add new ops to list
+        new_ops = generate_ops(new_ops_num)
+        ops_list += new_ops
+    return ops_list
+
+
+def generate_ops(num_ops: int) -> List[Operator]:
+    op_types = ["honest_high_l1", "honest_normal_l1", "honest_low_l1", "cheating_l1"]
+    op_types_probs = [0.1, 0.75, 0.1, 0.05]
+    ops_list = []
+    for i in range(num_ops):
+        op_type = np.random.choice(op_types, p=op_types_probs)
+        new_op = Operator(op_type)
+        ops_list.append(new_op)
+    return ops_list
+
+
+def update_ops_bandwidth(ops_list: List[Operator]) -> None:
+    for op in ops_list:
+        op.generate_bandwidth()
+
+
+def update_ops_rewards(
+    ops_list: List[Operator], day_pool: float, k1: float, k2: float
+) -> None:
+    op_bw_arr = np.array([op.bandwidth_list[-1] for op in ops_list])
+    op_speed_arr = np.array([op.speed_ratio for op in ops_list])
+    op_uptime_arr = np.array([op.uptime for op in ops_list])
+    reward_num = op_bw_arr**k1 * op_speed_arr**k2 * op_uptime_arr**k2
+    op_reward_arr = day_pool * reward_num / sum(reward_num)
+    for i, op in enumerate(ops_list):
+        op.add_reward(op_reward_arr[i])
+
+
+def update_ops_flags(ops_list: List[Operator]) -> None:
+    for op in ops_list:
+        op.generate_flag()
+
+
+def update_ops_payouts(ops_list: List[Operator]) -> None:
+    for op in ops_list:
+        op.compute_current_payout()
