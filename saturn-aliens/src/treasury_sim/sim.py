edbe5c718fe9384c0c98698b8d11f83eefa4e734
Maria Silva <misilva73@gmail.com>
2022-10-12 18:59:13 +0200

    Add simulation with growing daily reward pool

--- saturn-aliens/src/treasury_sim/sim.py
+++ saturn-aliens/src/treasury_sim/sim.py
@@ -19,17 +19,23 @@ def run_single_sim(
     day_pool_list: List[float],
     initial_ops_num: int,
     new_ops_num: int,
+    is_growth_pool: bool = False,
+    total_pool: float = None,
 ) -> List[Operator]:
     ops_list = generate_ops(initial_ops_num)
     for sim_step in range(sim_len):
-        # Get daily reward from initial pool
-        if sim_step < len(day_pool_list):
-            day_pool: float = day_pool_list[sim_step]
-        else:
-            day_pool: float = 0.0
         # Update perf. metrics and log detection flags
         update_ops_bandwidth(ops_list)
         update_ops_flags(ops_list)
+        # Get daily reward from initial pool
+        if is_growth_pool:
+            day_pool: float = compute_growth_day_pool(
+                sim_step, sim_len, total_pool, ops_list
+            )
+        elif sim_step < len(day_pool_list):
+            day_pool: float = day_pool_list[sim_step]
+        else:
+            day_pool: float = 0.0
         # Compute rewards (before penalties)
         update_ops_rewards(ops_list, day_pool, k1, k2, penalty_multiplier)
         # Compute final payout (after penalties)
@@ -39,6 +45,58 @@ def run_single_sim(
             new_ops = generate_ops(new_ops_num)
             ops_list += new_ops
     return ops_list
+
+
+def compute_growth_day_pool(
+    sim_step: int, sim_len: int, total_pool: float, ops_list: List[Operator]
+):
+    # Get current cummulive rewards
+    curr_bw_arr = np.array([op.get_total_bandwidth() for op in ops_list])
+    curr_cum_rewards_achieved = compute_cum_rewards(
+        sim_step, sim_len, total_pool, curr_bw_arr
+    )
+    # Get previous cummulive rewards
+    day_bw_arr = np.array([op.bandwidth_list[-1] for op in ops_list])
+    prev_bw_list = curr_bw_arr - day_bw_arr
+    prev_cum_rewards_achieved = compute_cum_rewards(
+        sim_step - 1, sim_len, total_pool, prev_bw_list
+    )
+    # Set day pool
+    day_pool = curr_cum_rewards_achieved - prev_cum_rewards_achieved
+
+    return day_pool
+
+
+def compute_cum_rewards(
+    sim_step: int, sim_len: int, total_pool: float, bw_arr: np.array
+):
+    # Get goal bandwidth variables
+    goal_committed_bw = get_goal_bandwidth(sim_step)
+    total_bandwidth_goal = get_goal_bandwidth(sim_len)
+    # Get commited bandwidth variables
+    total_committed_bw = sum(bw_arr)
+    # Get reward varibles
+    committed_bw_ratio = (
+        min(goal_committed_bw, total_committed_bw) / total_bandwidth_goal
+    )
+    cum_rewards = total_pool * committed_bw_ratio
+    return cum_rewards
+
+
+def get_goal_bandwidth(sim_step: int):
+    """
+    The bandwidth goal is computed with the following assumptions:
+        1. Each operator contributes on average 1 TB per day
+        2. We start with 50 operators
+        3. Five operators enter the network every day
+    """
+    if sim_step < 0:
+        goal_bw = 0.0
+    elif sim_step == 0:
+        goal_bw = 50.0
+    else:
+        goal_bw = (sim_step * 50.0) + (2.5 * sim_step * (sim_step + 1))
+    return goal_bw
 
 
 def generate_ops(num_ops: int) -> List[Operator]:
