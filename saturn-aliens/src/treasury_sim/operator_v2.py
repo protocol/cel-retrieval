5f36e92cc6637e891dad5961f622554902593668
Maria Silva <misilva73@gmail.com>
2022-10-19 18:20:25 +0100

    Add simulation with final parameters

---
+++ saturn-aliens/src/treasury_sim/operator_v2.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import numpy as np
+from typing import List, Tuple
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+MODULE_DIR = os.path.abspath(os.path.join(os.path.dirname(SCRIPT_DIR), ".."))
+sys.path.append(MODULE_DIR)
+
+from treasury_sim.operator import Operator
+
+
+class OperatorV2(Operator):
+    def set_performance(self) -> None:
+        # Ansgar estimates a bandwidth of 45TB/mo/node
+        if self.op_type in ["honest_high_l1", "cheating_l1"]:
+            self.mu_bandwidth: float = 1.2
+            self.sigma_bandwidth: float = 0.05
+            self.uptime: float = 1.0
+            self.speed_ratio: float = 1.0
+        elif self.op_type == "honest_normal_l1":
+            self.mu_bandwidth: float = 0.9
+            self.sigma_bandwidth: float = 0.05
+            self.uptime: float = 1.0
+            self.speed_ratio: float = 0.9
+        elif self.op_type == "honest_low_l1":
+            self.mu_bandwidth: float = 0.6
+            self.sigma_bandwidth: float = 0.05
+            self.uptime: float = 0.9
+            self.speed_ratio: float = 0.5
+        else:
+            raise ValueError("Invalid op_type")
